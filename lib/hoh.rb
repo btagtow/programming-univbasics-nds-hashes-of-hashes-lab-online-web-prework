7c114ffe146de430964dd913d440904f64dcb4f3
Ben Tagtow <bentagtow@gmail.com>
2019-11-08 21:37:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,50 +4,37 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+ PORTION_1 = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
+
+ PORTION_2 = {
+   label: "Order"
+ }
+ 
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+ 
+PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
 
 def naming_system
-  life = {
-    :label=> "Kingdom",
-    :sub_category=>{
-      :label=> "Phylum",
-      :sub_category=> {
-        :label=> "Class"
-      }
-    },
-    :label=> "Order", 
-    :label=> "Family",
-    :sub_category => {
-      :label=> "Genus"
-    },
-    :label=> "Species", 
-    :sub_category=> []
-  }
-  life
+  PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  PORTION_2[:sub_category] = PORTION_3
+  PORTION_3[:sub_category][:sub_category] =  PORTION_4
+  PORTION_1
     
   
   #  Class
