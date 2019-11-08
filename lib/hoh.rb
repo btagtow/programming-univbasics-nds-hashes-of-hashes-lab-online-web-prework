b208366e649807b778929c9f6f62726f8f68efdc
Ben Tagtow <bentagtow@gmail.com>
2019-11-08 21:31:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,20 +32,20 @@
 
 def naming_system
   life = {
-    label: "Kingdom",
-    sub_category: {
-      label: "Phylum",
-      sub_category: {
-        label: "Class"
+    :label=> "Kingdom",
+    :sub_category=> {
+      :label=> "Phylum",
+      :sub_category=> {
+        :label=> "Class"
       }
     },
-    label: "Order", 
-    label: "Family",
-    sub_category: {
-      label: "Genus"
+    :label=> "Order", 
+    :label=> "Family",
+    :sub_category => {
+      :label=> "Genus"
     },
-    label: "Species", 
-    sub_category: []
+    :label=> "Species", 
+    :sub_category=> []
   }
   life
     
